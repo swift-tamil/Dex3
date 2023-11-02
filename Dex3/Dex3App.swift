2fb92c7314ac4f264a9a0b64ca425d98d027e89c
MohamedGoush <0x1110111@gmail.com>
2023-11-02 07:43:05 +0530

    Initial Commit

---
+++ Dex3/Dex3App.swift
@@ -0,0 +1,20 @@
+//
+//  Dex3App.swift
+//  Dex3
+//
+//  Created by python on 02/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Dex3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
